befe9054538a2eb21b5ad60c9613ee75c5e4e997
HassanAhmed017 <hassan0ahmed17@gmail.com>
2026-05-03 22:33:46 +0300

    changed the process id from string to int

--- src/main/java/model/Process.java
+++ src/main/java/model/Process.java
@@ -2,7 +2,7 @@ package model;
 
 public class Process {
 
-    private String pid;
+    private int pid;
     private int arrivalTime;
     private int burstTime;
     private int priority;
@@ -11,14 +11,14 @@ public class Process {
     private int turnaroundTime;
     private int responseTime;
 
-    public Process(String pid, int arrivalTime, int burstTime, int priority) {
+    public Process(int pid, int arrivalTime, int burstTime, int priority) {
         this.pid = pid;
         this.arrivalTime = arrivalTime;
         this.burstTime = burstTime;
         this.priority = priority;
     }
 
-    public String getPid() { return pid; }
+    public int getPid() { return pid; }
     public int getArrivalTime() { return arrivalTime; }
     public int getBurstTime() { return burstTime; }
     public int getPriority() { return priority; }
