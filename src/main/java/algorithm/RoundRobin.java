f1e9b867f0b127b622591c6b043b172459f464e3
HassanAhmed017 <hassan0ahmed17@gmail.com>
2026-05-06 00:14:26 +0300

    updated RR and added it's algorithm

--- src/main/java/algorithm/RoundRobin.java
+++ src/main/java/algorithm/RoundRobin.java
@@ -5,11 +5,36 @@ import model.Process;
 public class RoundRobin {
     private Queue<Process> readyQRR = new LinkedList<>();
 
-    void startRR(int Q, ArrayList<Process> processes){
+    private int RRCnt = 0;//start of the process
+
+    void startRR(int quantum, ArrayList<Process> processes){
         initializeReadyQ(processes);
         System.out.println(readyQRR); //here we print ready queue for the first time (remove sout in gui)
 
-    
+        while (!readyQRR.isEmpty()){
+            if(RRCnt < readyQRR.peek().getArrivalTime()){
+                RRCnt = readyQRR.peek().getArrivalTime();
+            }
+
+
+            readyQRR.peek().setRemainigBurstTime(readyQRR.peek().getRemainigBurstTime()-quantum);
+            if(readyQRR.peek().getResponseTime() == -1){
+                readyQRR.peek().setResponseTime(RRCnt);
+            }
+
+            //turnAround & waiting times
+            if (readyQRR.peek().getRemainigBurstTime() == 0){
+                //TurnAroundTime
+                readyQRR.peek().setFinishTime(RRCnt+quantum);
+                readyQRR.peek().setTurnaroundTime(readyQRR.peek().getArrivalTime() - readyQRR.peek().getFinishTime());
+                //WaitingTime = finishTime - ArrivalTime - BurstTime
+                readyQRR.peek().setWaitingTime(readyQRR.peek().getFinishTime()-readyQRR.peek().getArrivalTime()-readyQRR.peek().getBrustTime());
+            }
+
+            RRCnt+=quantum;
+
+        }
+
     }
 
     void initializeReadyQ(ArrayList<Process> processes){
@@ -18,3 +43,6 @@ public class RoundRobin {
         }
     }
 }
+
+
+//write a function for PQ of same P
