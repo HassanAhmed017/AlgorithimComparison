d9e18b7fbf1208837d80d8f3a48ccb87e961014d
HassanAhmed017 <hassan0ahmed17@gmail.com>
2026-05-06 02:33:37 +0300

    updated the code by fixing some bugs, rearranging the order of some lines, and added printRQRR to print RQ in running time

--- src/main/java/algorithm/RoundRobin.java
+++ src/main/java/algorithm/RoundRobin.java
@@ -1,5 +1,7 @@
 package algorithm;
+
 import java.util.*;
+
 import model.Process;
 
 public class RoundRobin {
@@ -7,49 +9,71 @@ public class RoundRobin {
 
     private int RRCnt = 0;//start of the process
 
-    void startRR(int quantum, ArrayList<Process> processes){
+    Timer timer = new Timer();
+    TimerTask task = new TimerTask() {
+        @Override
+        public void run() {
+            printRQRR();
+            if (readyQRR.isEmpty()) {
+                timer.cancel();
+            }
+        }
+    };
+
+    void startRR(int quantum, ArrayList<Process> processes) {
         initializeReadyQ(processes);
         System.out.println(readyQRR); //here we print ready queue for the first time (remove sout in gui)
 
-        while (!readyQRR.isEmpty()){
-            if(RRCnt < readyQRR.peek().getArrivalTime()){
+
+        timer.schedule(task, 0, 10000); //used to print the RQ every 10 seconds during runtime
+
+        while (!readyQRR.isEmpty()) {
+            if (RRCnt < readyQRR.peek().getArrivalTime()) {
                 RRCnt = readyQRR.peek().getArrivalTime();
             }
 
-
-            int remaining = readyQRR.peek().getRemainigBurstTime();
-
-            if(remaining <= quantum) {
-                readyQRR.peek().setBrustTime(0);
-            } else {
-                readyQRR.peek().setRemainigBurstTime(remaining - quantum);
-            }
-
-            if(readyQRR.peek().getResponseTime() == -1){
+            if (readyQRR.peek().getResponseTime() == -1) {
                 readyQRR.peek().setResponseTime(RRCnt);
             }
 
-            //turnAround & waiting times
-            if (remaining == 0){
-                //TurnAroundTime
-                readyQRR.peek().setFinishTime(RRCnt+quantum);
-                readyQRR.peek().setTurnaroundTime(readyQRR.peek().getArrivalTime() - readyQRR.peek().getFinishTime());
-                //WaitingTime = finishTime - ArrivalTime - BurstTime
-                readyQRR.peek().setWaitingTime(readyQRR.peek().getFinishTime()-readyQRR.peek().getArrivalTime()-readyQRR.peek().getBrustTime());
+            int remaining = readyQRR.peek().getRemainigBurstTime();
+            if (remaining <= quantum) {
+                readyQRR.peek().setRemainigBurstTime(0);
+                RRCnt += remaining;
+            } else {
+                readyQRR.peek().setRemainigBurstTime(remaining - quantum);
+                RRCnt += quantum;
             }
 
-            RRCnt+=quantum;
+            if (readyQRR.peek().getRemainigBurstTime() == 0){
+                //TurnAroundTime
+                readyQRR.peek().setFinishTime(RRCnt);
+                readyQRR.peek().setTurnaroundTime(readyQRR.peek().getFinishTime() - readyQRR.peek().getArrivalTime());
+                //WaitingTime = finishTime - ArrivalTime - BurstTime
+                readyQRR.peek().setWaitingTime(readyQRR.peek().getFinishTime() - readyQRR.peek().getArrivalTime() - readyQRR.peek().getBrustTime());
+                readyQRR.remove();
+            }
+            else{
+                readyQRR.add(readyQRR.peek());
+                readyQRR.poll();
+            }
+            //turnAround & waiting times
+
 
         }
 
     }
 
-    void initializeReadyQ(ArrayList<Process> processes){
-        for(int i = 0 ; i < processes.size() ; i++){
+    void initializeReadyQ(ArrayList<Process> processes) {
+        for (int i = 0; i < processes.size(); i++) {
             readyQRR.add(processes.get(i));
         }
     }
+
+    void printRQRR() {
+        System.out.println(readyQRR);
+    }
+
+
 }
-
-
 //write a function for PQ of same P
