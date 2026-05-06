fa6b5d57977a3ea49dfd7954a35cddb16128147d
amany <amanymohamedeesalem@gmail.com>
2026-05-06 16:24:02 +0300

    refactor: simplify model by removing HashMap

--- src/main/java/model/ScheduleResult.java
+++ src/main/java/model/ScheduleResult.java
@@ -1,16 +1,11 @@
-package model;
+ package model;
 
 import java.util.List;
-import java.util.Map;
 
 public class ScheduleResult {
 
     private List<GanttBlock> ganttBlocks;
-
-    private Map<String, Integer> waitingTimes;
-    private Map<String, Integer> turnaroundTimes;
-    private Map<String, Integer> responseTimes;
-    private Map<String, Integer> completionTimes;
+    private List<Process> processes;
 
     private double avgWaitingTime;
     private double avgTurnaroundTime;
@@ -18,53 +13,22 @@ public class ScheduleResult {
 
     public ScheduleResult(
             List<GanttBlock> ganttBlocks,
-            Map<String, Integer> waitingTimes,
-            Map<String, Integer> turnaroundTimes,
-            Map<String, Integer> responseTimes,
-            Map<String, Integer> completionTimes,
+            List<Process> processes,
             double avgWaitingTime,
             double avgTurnaroundTime,
             double avgResponseTime
     ) {
         this.ganttBlocks = ganttBlocks;
-        this.waitingTimes = waitingTimes;
-        this.turnaroundTimes = turnaroundTimes;
-        this.responseTimes = responseTimes;
-        this.completionTimes = completionTimes;
+        this.processes = processes;
         this.avgWaitingTime = avgWaitingTime;
         this.avgTurnaroundTime = avgTurnaroundTime;
         this.avgResponseTime = avgResponseTime;
     }
 
-    public List<GanttBlock> getGanttBlocks() {
-        return ganttBlocks;
-    }
+    public List<GanttBlock> getGanttBlocks() { return ganttBlocks; }
+    public List<Process> getProcesses() { return processes; }
 
-    public Map<String, Integer> getWaitingTimes() {
-        return waitingTimes;
-    }
-
-    public Map<String, Integer> getTurnaroundTimes() {
-        return turnaroundTimes;
-    }
-
-    public Map<String, Integer> getResponseTimes() {
-        return responseTimes;
-    }
-
-    public Map<String, Integer> getCompletionTimes() {
-        return completionTimes;
-    }
-
-    public double getAvgWaitingTime() {
-        return avgWaitingTime;
-    }
-
-    public double getAvgTurnaroundTime() {
-        return avgTurnaroundTime;
-    }
-
-    public double getAvgResponseTime() {
-        return avgResponseTime;
-    }
+    public double getAvgWaitingTime() { return avgWaitingTime; }
+    public double getAvgTurnaroundTime() { return avgTurnaroundTime; }
+    public double getAvgResponseTime() { return avgResponseTime; }
 }
